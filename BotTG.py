0c7d1707d061497e82d9e80983ba7142325ac5e1
Vanya Bondarenko <113851939+Garelkaa@users.noreply.github.com>
2023-04-03 10:58:14 +0300

    Add files via upload

---
+++ BotTG.py
@@ -0,0 +1,34 @@
+import imp
+import logging
+from aiogram import Bot, Dispatcher, executor,types
+from db import Database
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token='5846206421:AAH5TG_JYsH6wAHDYsU3eGGH2M-4lhTH38I')
+dp = Dispatcher(bot)
+db = Database('database.db')
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if message.chat.type == 'private':
+            if not db.user_exists(message.from_user.id):
+                db.add_user(message.from_user.id)
+            await bot.send_message(message.from_user.id, "Добро пожаловать!") 
+
+@dp.message_handler(commands=['рассылка'])
+async def sendall(message: types.Message):
+    if message.chat.type == 'private':
+        if message.from_user.id == 626452615:
+            text = message.text[10:]
+            users = db.get_users()
+            for row in users:
+                try:
+                    await bot.send_message(row[0], text)
+                    if int(row[1]) != 1:
+                        db.set_active(row[0], 1)
+                except:
+                    db.set_active(row[0], 0)
+            await bot.send_message(message.from_user.id, "Успех")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates= True)
